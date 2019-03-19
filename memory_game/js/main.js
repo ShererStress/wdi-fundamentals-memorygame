b33b04d08ffa2c83b9b9705263e0ece5c7485ce7
ShererStress <ryanswhitehill@gmail.com>
2019-03-18 19:00:17 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,31 +24,51 @@ var cards = [{
 var cardsInPlay = [];
 
 //Function definitions
-var checkForMatch = function() {
 
+//Checks if the two cards are a match, then resets the gamestate automatically
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log('You found a match!');
+    alert('You found a match!');
+    var currentScore = parseInt(document.getElementById("scoreBoard").innerHTML); //Get current score
+    document.getElementById("scoreBoard").innerHTML = currentScore+1; //increment the score by +1
   } else {
-    console.log('Sorry, try again.');
+    alert('Sorry, try again.');
   }
+  resetBoard()
+  cardsInPlay = [];
 }
 
-var flipCard = function(cardId) {
-
+//See the card that was selected
+var flipCard = function() {
+  cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
-  console.log("the suit is of " + cards[cardId].suit);
-  console.log("Its image is at " + cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
-
+  this.setAttribute("src", cards[cardId].cardImage);
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
+//Creates a fresh boardstate, with unrevealed cards
+var createBoard = function() {
+  for (i=0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+//Clears all cards, then calls 'createBoard'
+var resetBoard = function() {
+  var cardsToDelete = document.getElementById("game-board");
+  while (cardsToDelete.hasChildNodes()) {
+    cardsToDelete.removeChild(cardsToDelete.childNodes[0]);
+  }
+  createBoard();
+}
+
+
 //Lines to run
-
-flipCard(0);
-flipCard(2);
-
-
-//"queen", "queen", "king", "king"
+createBoard();
